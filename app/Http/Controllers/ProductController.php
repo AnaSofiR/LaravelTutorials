9bce3c10ba63d78dbf618970129252a17cae7f6b
AnaSofiR <asrodriguo@eafit.edu.co>
2024-08-24 23:06:05 -0500

    Tutorial 2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,37 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use App\Models\Product;
 
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>80  ],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>150  ],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>50  ],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>40  ]
-    ];
+
 
     public function index(): View
     {
         $viewData = [];
         $viewData["title"] = "Products - Online Store";
         $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = ProductController::$products;
+        $viewData["products"] = Product::all();
         return view('product.index')->with("viewData", $viewData);
     }
 
     public function show(string $id) : View | RedirectResponse
     {
 
+
+        /*
         if (!isset(ProductController::$products[$id-1])) {
             return redirect()->route('home.index');
         }
+        */
 
         
 
         $viewData = [];
-        $product = ProductController::$products[$id-1];
+        $product = Product::findOrFail($id);
         $viewData["title"] = $product["name"]." - Online Store";
         $viewData["subtitle"] =  $product["name"]." - Product information";
         $viewData["product"] = $product;
@@ -51,16 +50,16 @@ class ProductController extends Controller
         return view('product.create')->with("viewData",$viewData);
     }
 
-    public function save(Request $request): View
+    public function save(Request $request): RedirectResponse
     {
         $request->validate([
             "name" => "required",
             "price" => ["required", "gt:0"]
         ]);
 
-        // Aquí iría el código para guardar el producto en la base de datos
-        //dd($request->all());
-        return view('product.save');
+        Product::create($request->only(["name","price"]));
+
+        return back();
         
     }
 
